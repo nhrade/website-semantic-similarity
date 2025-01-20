ae5794670cb0c9392ee96c9e3aa2eefc33b296a7
Noah Hradek <noahh717@gmail.com>
2025-01-20 00:32:08 +0000

    App changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,44 @@
 import streamlit as st
+from bs4 import BeautifulSoup
+import requests
+import spacy
 
-st.title("🎈 My new app")
+nlp = spacy.load("en_core_web_md")
+
+
+def check_similarity(url1, url2):
+    website1_data = requests.get(url1).text
+    website2_data = requests.get(url2).text
+    text1 = BeautifulSoup(website1_data, "html.parser").get_text()
+    text2 = BeautifulSoup(website2_data, "html.parser").get_text()
+    doc1 = nlp(text1)
+    doc2 = nlp(text2)
+    sim = doc1.similarity(doc2)
+    return sim
+
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    """
+# Website Similarity Tool
+
+This tool lets you determine the semantic similarity of two webpages.
+Enter the URLs below and then click "Check Similarity" to determine by how much each
+website is similar.
+         """
 )
+
+col1, col2 = st.columns(2)
+
+with col1:
+    left_url = st.text_input(label="First URL", placeholder="https://en.wikipedia.org/wiki/Shiva")
+
+with col2:
+    right_url = st.text_input(label="Second URL", placeholder="https://en.wikipedia.org/wiki/Yoga")
+
+similarity = 0
+
+if st.button("Check Similarity", type="primary"):
+    with st.spinner("Please wait..."):
+        similarity = round(check_similarity(left_url, right_url), 4)
+
+st.metric("Similarity", f"{similarity*100}%")
